d0e467e72fe2b60768abc469c0fd28990e0b183b
Uma Nandyala <18457369+nandyalu@users.noreply.github.com>
2025-01-18 14:04:04 -0500

    Cleanup start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,6 @@ echo "Running application as user: $(whoami)"
 # Start the script
 echo "Starting MTDP application"
 cd /app
-# exec uvicorn backend.main:trailarr_api --host 0.0.0.0 --port ${APP_PORT:-7889}
 
 # Add apscheduler to run the script every 60 minutes
 # Run python script tasks.schedules 
